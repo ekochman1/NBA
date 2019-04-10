b2f549c50f616af967d943fabec41ce846f8f286
ewcvis <35750299+ewcvis@users.noreply.github.com>
2019-04-09 19:18:45 -0700

    The springboot/maven files for login and register
    Still needs some optimizing, it probably has some unnecessary features, and I have yet to implement the hashing function

---
+++ NBADraftProject/src/main/java/NBADraftProject/NBADraftProject/MyServer.java
@@ -0,0 +1,19 @@
+package NBADraftProject.NBADraftProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.HashMap;
+
+
+@SpringBootApplication
+public class MyServer {
+
+	public static HashMap<String, String> users = new HashMap<>();
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyServer.class, args);
+		
+
+	}
+
+}
