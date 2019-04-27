39fc27e8eae1d141addf61433134c13382e55b30
ewcvis <35750299+ewcvis@users.noreply.github.com>
2019-04-27 15:26:42 -0700

    added draft functions and json displays
    Added an array of JSON Arrays, still needs to finalize finish Draft, but currently it has a copy of the Database in a JSON file and has modifies a separate version for users to see
    
    Also added the ability to go to the website without having to type the html in the url

--- NBADraftProject/src/main/java/NBADraftProject/NBADraftProject/UserController.java
+++ NBADraftProject/src/main/java/NBADraftProject/NBADraftProject/UserController.java
@@ -3,6 +3,7 @@ package NBADraftProject.NBADraftProject;
 import java.security.MessageDigest;
 
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 import org.springframework.http.*;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 
@@ -15,44 +16,151 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import org.json.JSONObject;
 import org.json.JSONArray;
 
+
 @RestController
 public class UserController {
-    static int count = 0; // this is bad cause this int is not thread safe, so it would not be consistence across multiple users
+	private static ArrayList<JSONArray> MasterJSON;
+	
+	
     //same logic behind databases, imagine the wallet system, but you only have 5 dollars and you get charged 3 dollars at the same time, in theory you are capable to handling each one individually
     //but not both
 
-
-    /*
-    //create a transaction
-    @RequestMapping(value = "/draft", method = RequestMethod.GET)
-    public ResponseEntity<String> draft(@RequestBody String payload, HttpServletRequest request){
+    
+    //create a transaction 
+    /* this will be to finish the draft
+    @RequestMapping(value = "/FinishDraft", method = RequestMethod.GET)
+    public ResponseEntity<String> FinishDraft(@RequestBody String payload, HttpServletRequest request){
         JSONObject payloadObj = new JSONObject(payload);
         String playerName = payloadObj.getString("playerName");
+        String leagueID = payloadObj.getString("leagueID"); //Grabbing name and age parameters from URL
+        String playerID = payloadObj.getString("playerID"); //just a fancier name for their username 
+
+		//Creating http headers object to place into response entity the server will return.
+		//This is what allows us to set the content-type to application/json or any other content-type
+		//we would want to return 
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set("Content-Type", "application/json");
+
+        
         try {
             String nameToPull = request.getParameter("username");
             Connection conn = DriverManager.getConnection("nbafantasydb.cxa7g8pzkm2m.us-east-2.rds.amazonaws.com" , "root", "Ethaneddie123");
             String SearchQuery = "SELECT owners  FROM Users WHERE owners = ?";
             PreparedStatement state = null;
+            state = conn.prepareStatement("START TRANSACTION");
+            state.execute();
             state = conn.prepareStatement(SearchQuery);
             state.setString(1, nameToPull);
             ResultSet rs = state.executeQuery();
-            boolean isNew = true;
-
+            while (rs.next()) {
+                if (rs.getString("owner").equals(playerID)) { //finds the user and gets their database
+                	  //insert new connection to the table that holds the league IDs, userIDS and the player they wanted to draft
+                	  // connect to change 
+                	
+                }
+            }
         }
 
         catch(SQLException e){
 
+                  e.printStackTrace();
+
+                  return new ResponseEntity("Check status of server for stack trace.", responseHeaders, HttpStatus.BAD_REQUEST);
 
         }
 
-    }
+    }   */
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public RedirectView landing() {
+		return new RedirectView("/LoginWorkspace.html");
+	}
+    
+    @RequestMapping(value = "/createDraft", method = RequestMethod.GET)
+ // <-- setup the endpoint URL at /hello with the HTTP POST method
+ public ResponseEntity<String> database(HttpServletRequest leagueID) {
+     //String nameToPull = request.getParameter("firstname");
+    	
+    	String JSONID = leagueID.getParameter("leagueID"); 
+    	
+     HttpHeaders responseHeaders = new HttpHeaders();
+     responseHeaders.set("Content-Type", "application/json");
+     
+     Connection conn = null;
+     ArrayList<String> listdata = new ArrayList<String>();
+     JSONArray nameArray = new JSONArray();
+     int playerId = 0;
+     String position = "";
+     int Player_rankPos = 0;
+     int Player_rankOverall = 0;
+     String name = "";
+     String team = "";
+     boolean taken = false;
+    // double Player_standDev = 0; never used?
 
+     try {
+         conn = DriverManager.getConnection("jdbc:mysql://nbafantasydb.cxa7g8pzkm2m.us-east-2.rds.amazonaws.com/NBAFantasy", "root", "Ethaneddie123");
+         String query = "SELECT playerId, position, Player_rankPos, Player_rankOverall, name, team FROM Player_Ranking";
+         PreparedStatement stmt = null;    //important for safety reasons
+         /**String tester = "tester";
+          return tester;**/
+
+         stmt = conn.prepareStatement(query);
+         // stmt.setString(1, nameToPull);
+         ResultSet rs = stmt.executeQuery();
+         while (rs.next()) {    //while there's something else next in the result set
+             playerId = rs.getInt("playerId");
+             position = rs.getString("position");
+             Player_rankPos = rs.getInt("Player_rankPos");
+             Player_rankOverall = rs.getInt("Player_rankOverall");
+             name = rs.getString("name");
+             team = rs.getString("team");
+
+             JSONObject obj= new JSONObject();
+             obj.put("leagueid", JSONID); //this way i can identify the master JSON file
+             obj.put("playerId", playerId);
+             obj.put("position", position);
+             obj.put("Player_rankPos", Player_rankPos);
+             obj.put("Player_rankOverall", Player_rankOverall);
+             obj.put("name", name);
+             obj.put("team", team);
+             obj.put("taken", taken);
+             
+             nameArray.put(obj);
+
+             if (nameArray != null) {
+                 for (int i=0;i<nameArray.length();i++){
+                     listdata.add(nameArray.getString(i));
+                 }
+             }
+             
+            //puts the json array in the EC2 server
+         }
+         MasterJSON.add(nameArray); 
+     } catch (SQLException e) {
+         return new ResponseEntity(e.toString(), responseHeaders, HttpStatus.OK);
+     } finally {
+         try {
+             if (conn != null) {
+                 conn.close();
+             }
+         } catch (SQLException se) {
+         }
+     }
+     return new ResponseEntity(nameArray.toString(), responseHeaders, HttpStatus.OK);
+
+ }
+
+ /*
     @RequestMapping(value = "/createDraft", method = RequestMethod.POST)
     public ResponseEntity<String> createDraft(@RequestBody String userID, HttpServletRequest request){
+    	
+    	
         try {
             Connection conn = DriverManager.getConnection("nbafantasydb.cxa7g8pzkm2m.us-east-2.rds.amazonaws.com" , "root", "Ethaneddie123");
             String SearchQuery = "SELECT owners FROM Users WHERE owners = ?";
@@ -60,18 +168,84 @@ public class UserController {
             state = conn.prepareStatement(SearchQuery);
             state.setString(1, userID);
             ResultSet rs = state.executeQuery();
-
+            
 
         }
 
         catch(SQLException e){
+            e.printStackTrace();
 
 
         }
 
-    }
+    } */
+    
+    @RequestMapping(value = "/draft", method = RequestMethod.POST)  //THIS SHOULD BE ACTIVATED ON CLICK
+    public ResponseEntity<String> draft(@RequestBody String payload, HttpServletRequest request){
+    	JSONObject payloadObj = new JSONObject(payload);
+    	int leagueID = payloadObj.getInt("leagueID");
+    	int PlayerID = payloadObj.getInt("playerID");
+    	//String position = payloadObj.getString("position");
+    	//int Player_rankPos = payloadObj.getInt("Player_rankPos");
+    	//int Player_rankOverall = payloadObj.getInt("Plater_rankOverall");
+    	//String name = payloadObj.getString("name");
+    	//String team = payloadObj.getString("team");
+    	JSONArray newArray = new JSONArray();
+    	JSONArray nameArray = new JSONArray();
+    	
+    	  HttpHeaders responseHeaders = new HttpHeaders();
+    	  responseHeaders.set("Content-Type", "application/json");
+    	
+    	
+    	for(int i = 0; i < MasterJSON.size(); i++) {
+    		if(MasterJSON.get(i).getJSONObject(0).getInt("leagueid") == leagueID) {
+    			for(int j = 0; j <MasterJSON.get(i).length(); i++) {
+    				if(MasterJSON.get(i).getJSONObject(j).getInt("playerid") == PlayerID) {
+    			JSONObject obj = new JSONObject();
+    		    obj.put("leagueid", MasterJSON.get(i).getJSONObject(j).getInt("leagueid")); //this way i can identify the master JSON file
+                obj.put("playerId", MasterJSON.get(i).getJSONObject(j).getInt("playerID"));
+                obj.put("position", MasterJSON.get(i).getJSONObject(j).getString("position"));
+                obj.put("Player_rankPos", MasterJSON.get(i).getJSONObject(j).getInt("Player_rankPos"));
+                obj.put("Player_rankOverall", MasterJSON.get(i).getJSONObject(j).getInt("Player_rankOverall"));
+                obj.put("name", MasterJSON.get(i).getJSONObject(j).getString("name"));
+                obj.put("team", MasterJSON.get(i).getJSONObject(j).getString("team"));
+                obj.put("taken", true);
+                newArray.put(obj);
+    				}
+    			else {
+    					JSONObject obj = new JSONObject();
+    	    		    obj.put("leagueid", MasterJSON.get(i).getJSONObject(j).getInt("leagueid")); //this way i can identify the master JSON file
+    	                obj.put("playerId", MasterJSON.get(i).getJSONObject(j).getInt("playerID"));
+    	                obj.put("position", MasterJSON.get(i).getJSONObject(j).getString("position"));
+    	                obj.put("Player_rankPos", MasterJSON.get(i).getJSONObject(j).getInt("Player_rankPos"));
+    	                obj.put("Player_rankOverall", MasterJSON.get(i).getJSONObject(j).getInt("Player_rankOverall"));
+    	                obj.put("name", MasterJSON.get(i).getJSONObject(j).getString("name"));
+    	                obj.put("team", MasterJSON.get(i).getJSONObject(j).getString("team"));
+    	                obj.put("taken", MasterJSON.get(i).getJSONObject(j).getBoolean("taken"));
+    	                newArray.put(obj);
+    	    				}
+    				}
+    			
+                }
+    			MasterJSON.set(i, newArray);
+    			
+    			for(int k = 0; k < newArray.length(); k++ ) {
+    				if(!newArray.getJSONObject(k).getBoolean("taken")) {
+    					nameArray.put(newArray.getJSONObject(k));
+    				}
+    				
+    			}
+    			
+    			return new ResponseEntity(nameArray.toString(), responseHeaders, HttpStatus.OK); // MAKE SURE THAT WHEN THIS RETURNS THE OK STATUS, IT BROADCASTS THE NEW JSON OBJECT
+    			}
+		return new ResponseEntity("Something Went Wrong", responseHeaders, HttpStatus.OK);
+    		
+    		
+    	}
+    			
+    			
+    
 
-    */
     @RequestMapping(value = "/register", method = RequestMethod.POST)
     // <-- setup the endpoint URL at /hello with the HTTP POST method
     public ResponseEntity<String> register(@RequestBody String payload, HttpServletRequest request) { //springboot has a thread pool that handles the server with multiple users
