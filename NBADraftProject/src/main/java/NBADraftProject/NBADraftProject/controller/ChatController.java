cbddbdcbf86175771259214ae6552441b74c9b23
Unknown <jcurley@oxy.edu>
2019-05-05 00:07:08 -0700

    edit to chat controller

--- NBADraftProject/src/main/java/NBADraftProject/NBADraftProject/controller/ChatController.java
+++ NBADraftProject/src/main/java/NBADraftProject/NBADraftProject/controller/ChatController.java
@@ -2,20 +2,14 @@ package NBADraftProject.NBADraftProject.controller;
 
 import NBADraftProject.NBADraftProject.model.ChatMessage;
 import NBADraftProject.NBADraftProject.model.DraftMessage;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ChatController {
-    @Autowired
-    private SimpMessagingTemplate simpMessagingTemplate;
 
     @MessageMapping("/draft.sendMessage.{leagueID}")
 	@SendTo("/draft.{leagueID}")
